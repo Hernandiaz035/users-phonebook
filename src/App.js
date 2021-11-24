2a598dc5a4ebec35313f58b0d5a85009abc2e203
Hernan Diaz <hernandiaz035@gmail.com>
2021-11-24 21:24:33 +1100

    Add submit method to form and another Card with Users.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import useForm from './hooks/useForm'
 import Input from './components/Input'
 import Card from './components/Card.js'
@@ -7,38 +8,63 @@ import Button from './components/Button'
 const initialForm = { name: "", lastname: "", email: "" }
 
 const App = () => {
-  const [form, handleChange] = useForm(initialForm);
+  const [users, setUsers] = useState([])
+  const [form, handleChange, resetForm] = useForm(initialForm);
 
-  console.log(form)
+  const submit = (e) => {
+    e.preventDefault()
+
+    const tmpUsers = [...users, { ...form }]
+    const sortedUsers = tmpUsers.sort((a, b) => {
+      const aName = a.name.toLowerCase()
+      const bName = b.name.toLowerCase()
+
+      return aName < bName ? -1 : 1
+    })
+    setUsers(sortedUsers)
+    resetForm()
+  }
 
   return(
     <Container>
       <Card>
         <div style={{ padding: 20 }}>
-          <form>
+          <form onSubmit={submit}>
             <Input
               name="name"
               value={form.name}
               onChange={handleChange}
               label="Name"
-            />
+              placeholder="Name"
+              />
             <Input
               name="lastname"
               value={form.lastname}
               onChange={handleChange}
               label="Last name"
-            />
+              placeholder="Last name"
+              />
             <Input
               name="email"
               value={form.email}
               onChange={handleChange}
               label="Email"
+              placeholder="Email"
             />
             <Button>
               Submit
             </Button>
           </form>
         </div>
+      </Card>
+      <Card>
+        <ul>
+          {users.map((u) =>
+            <li key={u.email}>
+              {`Name: ${u.name}, Lastname: ${u.lastname}, Email: ${u.email}`}
+            </li>
+          )}
+        </ul>
       </Card>
     </Container>
   )
